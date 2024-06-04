b80f9c7a0120fa4e71b65a7e5f9c80163a60848f
Gargamel1569 <vianoce@jebalsomta.art>
2024-06-04 15:10:49 +0200

    Update constants.ts
    Signed-off-by: Gargamel1569 <vianoce@jebalsomta.art>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "https://api.ree6.de"
-export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.ree6.de"
+export const BASE_PATH = import.meta.env.VITE_API_URL || "https://discord.com/oauth2/authorize?client_id=1146091634203824140"
+export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://discord.com/oauth2/authorize?client_id=1146091634203824140&permissions=8&scope=bot"
 
 export function get(path: string) {
 
